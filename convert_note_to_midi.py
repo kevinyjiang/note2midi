e316476dc0957429bef37817948f17ef36f9d79d
Kevin Jiang <kj.kevin@gmail.com>
2018-11-15 03:27:01 -0500

    Create text file to avoid re-generating mapping

--- convert_note_to_midi.py
+++ convert_note_to_midi.py
@@ -3,39 +3,46 @@ Note to MIDI Number converter
 
 Author: Kevin Jiang
 Disclaimer: this probably already exists but YOLO
-
 """
 
-import sys
+import sys, os.path
 
 # Constants
-NOTES = ['C','D','E','F','G','A','B']
-ACCIDENTALS = ['b', '']
-NON_ACCIDENTAL_NOTES = ['F', 'C']
+NOTES = ['C','D','E','F','G','A','B'] 
+ACCIDENTALS = ['b', ''] # Flat and Natural (sharps are for plebs)
+NON_ACCIDENTAL_NOTES = ['F', 'C'] # F and C are not usually referred to as E# and B# (unless ur a pleb)
 
-# Returns mapping from notes (i.e. C6) to MIDI indexes
+# Returns mapping from notes (i.e. C6, G9, A0) to MIDI indexes
 def generate_mapping():
-	midi_idx = 12
-	note_to_midi_dict = {}
+	# Notes C0 to Ab0 will probably never be indexed, but generated for convenience
+	midi_idx = 12 
+	res = {}
 
 	for octave in range(10):
 		for note in NOTES:
 			if note not in NON_ACCIDENTAL_NOTES:
 				for accidental in ACCIDENTALS:
 					curr = note + accidental + str(octave)
-					note_to_midi_dict[curr] = midi_idx
+					res[curr] = midi_idx
 					midi_idx += 1
 			else: # F or C
 				curr = note + str(octave)
-				note_to_midi_dict[curr] = midi_idx
+				res[curr] = midi_idx
 				midi_idx += 1
 
-	return note_to_midi_dict
+	return res
 
 if __name__ == "__main__":
 	input_notes = sys.argv[1:]
-	mapping = generate_mapping()
 
+	if os.path.isfile("note2midi_map"):
+		with open("note2midi_map", 'r') as f:
+			mapping = eval(f.read())
+	else:
+		with open("note2midi_map", 'w') as f:
+			mapping = generate_mapping()
+			f.write(str(mapping))
+		
 	res = []
 
 	for note in input_notes:
